375b981ca34f7c21cfbbbcaacfb8eeb3288d5d79
HSA-Student <student.k.jama29@horizoncolumbus.org>
2021-03-25 10:58:32 -0400

    fix it

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.methods == "POST":
+    if request.method == "POST":
         friendship_percent = random.randint(1, 101)
-    friendship_percent = str(friendship_percent)
-    return render_template('home.html', friendship=(friendship_percent + "%"))
+        friendship_percent = str(friendship_percent)
+        return render_template('home.html', friendship=(friendship_percent + "%"))
+    else:
+        return render_template('home.html')
 
 
 if __name__ == '__main__':
